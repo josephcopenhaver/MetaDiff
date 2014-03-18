c2a9c47a0971cb0320b4f19d1868f371f4bcc132
Joseph Patrick Copenhaver <joseph.copenhaver@gmail.com>
2014-03-18 03:48:08 -0500

    added database init sequences.

--- metaDiff.pl
+++ metaDiff.pl
@@ -17,6 +17,65 @@ use constant
 	TYPE_SYMLINK => (1 << 2)
 };
 
+use constant
+{
+	SCRIPT_CREATE_PATH_NAMES_TABLE => 'CREATE TABLE path_names(name_id integer PRIMARY KEY NOT NULL, name TEXT NOT NULL UNIQUE);',
+	SCRIPT_CREATE_SNAPSHOT_DB_BASIS => 'CREATE TABLE path_elements(element_id integer PRIMARY KEY NOT NULL, type_id integer NOT NULL REFERENCES path_types(type_id) ON DELETE CASCADE, name_id integer NOT NULL REFERENCES path_names(name_id) ON DELETE CASCADE);
+CREATE TABLE element_parents(element_id integer PRIMARY KEY NOT NULL, parent_id integer REFERENCES path_elements(element_id) ON DELETE CASCADE, FOREIGN KEY(element_id) REFERENCES path_elements(element_id) ON DELETE CASCADE); --NULL parent indicates root level
+CREATE TABLE element_hash(element_id integer PRIMARY KEY NOT NULL, hash TEXT NOT NULL, FOREIGN KEY(element_id) REFERENCES path_elements(element_id) ON DELETE CASCADE);
+CREATE TABLE element_lastmodified(element_id integer PRIMARY KEY NOT NULL, last_modified TEXT NOT NULL, FOREIGN KEY(element_id) REFERENCES path_elements(element_id) ON DELETE CASCADE);
+CREATE TABLE element_size(element_id integer PRIMARY KEY NOT NULL, size integer CHECK(size>=0), FOREIGN KEY(element_id) REFERENCES path_elements(element_id) ON DELETE CASCADE);
+CREATE TABLE element_link(element_id integer PRIMARY KEY NOT NULL, target TEXT NOT NULL, FOREIGN KEY(element_id) REFERENCES path_elements(element_id) ON DELETE CASCADE);',
+	COMPARABLE_TABLES => [
+		'path_elements',
+		'element_parents',
+		'element_hash',
+		'element_lastmodified',
+		'element_size',
+		'element_link'
+	]
+};
+
+use constant
+{
+	SCRIPT_CREATE_META_INFO_TABLE => sprintf("%s\n%s", 'CREATE TABLE path_types(type_id integer PRIMARY KEY NOT NULL, type_description TEXT UNIQUE);', SCRIPT_CREATE_PATH_NAMES_TABLE)
+};
+
+use constant
+{
+	SCRIPT_CREATE_SNAPSHOT_DB => sprintf("%s%s", SCRIPT_CREATE_META_INFO_TABLE, SCRIPT_CREATE_SNAPSHOT_DB_BASIS),
+	SCRIPT_INIT_SNAPSHOT_DB => sprintf('INSERT INTO path_types SELECT %d AS type_id, \'directory\' AS type_description UNION SELECT %d,\'file\' UNION SELECT %d,\'symlink\';', TYPE_DIR, TYPE_FILE, TYPE_SYMLINK)
+};
+
+my $createSnap2Tables = [];
+my $renameSnap1Tables = [];
+
+foreach my $line (split(/\r?\n/, SCRIPT_CREATE_SNAPSHOT_DB_BASIS))
+{
+	foreach (@{+COMPARABLE_TABLES})
+	{
+		$line =~ s/(\Q$_\E)/$1.'2'/e;
+	}
+	push(@$createSnap2Tables, $line);
+}
+$createSnap2Tables = join("\n", @$createSnap2Tables);
+foreach (@{+COMPARABLE_TABLES})
+{
+	push(@$renameSnap1Tables, sprintf("ALTER TABLE %s RENAME TO %s2;", $_, $_));
+}
+$renameSnap1Tables = join("\n", @$renameSnap1Tables);
+
+use constant
+{
+	SCRIPT_CREATE_SNAPSHOT_2_TABLES => $createSnap2Tables,
+	SCRIPT_RENAME_SNAPSHOT_1_TABLES => $renameSnap1Tables
+};
+$createSnap2Tables = undef;
+$renameSnap1Tables = undef;
+
+
+###
+
 
 my $CSV_FD = undef;
 my $numArgv = scalar(@ARGV);
@@ -147,7 +206,9 @@ sub gatherElementInfo
 
 sub initDB
 {
-	#TODO: complete
+	my $dbh = $_[0];
+	$dbh->do(SCRIPT_CREATE_SNAPSHOT_DB) || die;
+	$dbh->do(SCRIPT_INIT_SNAPSHOT_DB) || die;
 }
 
 sub newRamDB
@@ -156,7 +217,7 @@ sub newRamDB
 		"dbi:SQLite:dbname=:memory:", # DSN
 		"", # user
 		"", # password
-		{ RaiseError => 1 } # other
+		{ RaiseError => 1, AutoCommit => 0 } # other
 	) || die;
 	
 	initDB($dbh);
@@ -225,6 +286,7 @@ sub getDirSnapshot
 		gatherElementInfo($abs_src_root_path);
 		$cleanupCsv->();
 		print("Saving data to file... ");
+		$dbh->commit() || die;
 		$dbh->sqlite_backup_to_file($out_file_path) || die;
 		print("Done\n");
 	};
